76fa154d281da777c9c0f1e3a58eaa0af38ddc76
Josemario01 <87789401+Josemario01@users.noreply.github.com>
2026-06-16 23:39:07 -0600

    Add files via upload

---
+++ Tarea1/src/main/java/com/project/demo/logic/entity/category/Category.java
@@ -0,0 +1,20 @@
+package com.project.demo.logic.entity.category;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "category")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+}
---
+++ Tarea1/src/main/java/com/project/demo/logic/entity/rol/RolRepository.java
@@ -0,0 +1,6 @@
+package com.project.demo.logic.entity.rol;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RolRepository extends JpaRepository<Rol, Long> {
+}
